af60ad88b9d1ab66554ca19d3cebe4bf74539f39
moon5ckq <moon5ckq@gmail.com>
2014-11-21 01:05:52 +0800

    add parse_int

--- friend_list/get_info.py
+++ friend_list/get_info.py
@@ -11,6 +11,11 @@ def save_file(path, url):
     urllib.urlretrieve(url, filepath)
     return filepath
 
+def parse_int(s):
+    if s.isdigit():
+        return int(s)
+    return -1
+
 with open(sys.argv[1]) as f:
     for url in f:
         d = pq(url = url.strip())
@@ -18,7 +23,7 @@ with open(sys.argv[1]) as f:
         name = d('#js_wikidb_main_name').text()
         norm = map(lambda x:d(x).text().split(' ')[-1], d('.ui_wikidb_top_area p'))
         attr = map(lambda x: d(x).text().split(' ')[-1], d('.ui_wikidb_middle_area p'))
-        correction = map(lambda x: int(x.strip().split()[-1]), \
+        correction = map(lambda x: parse_int(x.strip().split()[-1]), \
             d('.ui_wiki_db_bottom_wrapper  .db_other_text:first').text().strip('%').split('%'))
         q_avatar = save_file( 'q_avatar', d('.db_other_text a').attr('href') )
         m_avatar = save_file( 'm_avatar', d('.ui_wiki_db_main_img_area a').attr('href') )
